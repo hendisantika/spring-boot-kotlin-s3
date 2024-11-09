6f21a5d0da502944d02de133742cc086c96d9eb8
hendisantika <hendisantika@yahoo.co.id>
2024-11-09 07:42:07 +0700

    Add BucketController🫸🌀✏️📗🐧🐳⬆

--- src/main/kotlin/id/my/hendisantika/uploads3/controller/BucketController.kt
+++ src/main/kotlin/id/my/hendisantika/uploads3/controller/BucketController.kt
@@ -1,5 +1,9 @@
 package id.my.hendisantika.uploads3.controller
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.s3.S3Client
+
 /**
  * Created by IntelliJ IDEA.
  * Project : upload-s3
@@ -17,4 +21,6 @@ data class BucketRequest(val bucketName: String)
 
 data class ObjectRequest(val objectName: String, val content: String)
 
-class BucketController
+@RestController
+@RequestMapping("/buckets")
+class BucketController(private val s3Client: S3Client)
