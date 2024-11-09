08df3cbb202d90fa3b024bbe263c51a3cf83dd7d
hendisantika <hendisantika@yahoo.co.id>
2024-11-09 07:38:50 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/kotlin/id/my/hendisantika/uploads3/SpringBootKotlinS3Application.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.uploads3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinS3Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinS3Application>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/uploads3/SpringBootKotlinS3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.uploads3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinS3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
