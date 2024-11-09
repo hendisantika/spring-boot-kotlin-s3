f960a7a54667c0eba18644a121a51dd9a5357376
hendisantika <hendisantika@yahoo.co.id>
2024-11-09 07:41:11 +0700

    Add PutObjectRequestBody🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/kotlin/id/my/hendisantika/uploads3/controller/BucketController.kt
@@ -0,0 +1,16 @@
+package id.my.hendisantika.uploads3.controller
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : upload-s3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/11/24
+ * Time: 07.40
+ * To change this template use File | Settings | File Templates.
+ */
+
+typealias PutObjectRequestBody = software.amazon.awssdk.core.sync.RequestBody
+
+class BucketController
